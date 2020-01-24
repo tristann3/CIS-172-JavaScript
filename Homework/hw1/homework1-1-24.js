3ffd13bffa12b0c5ab6503de58fd73fd8bc8e0b1
Tristan Thompson <tristan15thompson@hawkmail.hfcc.edu>
2020-01-24 14:32:58 -0500

    Homework 1 done
    Tristan Thompson
    1/24/2020

--- Homework/hw1/homework1-1-24.js
+++ Homework/hw1/homework1-1-24.js
@@ -1,12 +1,40 @@
+/*
+Homework 1
+1/24/2020
+Tristan Thompson
+v1.0.0
+ */
+
 let myName = "Tristan";
-let myMajor = "Assicisates in Applied Science in Computer Science";
+let myMajor = "Associates in Applied Science in Computer Science";
 let funFact = "I have the same name as a professional athlete.";
 
 console.log(myName);
 console.log(myMajor);
 console.log(funFact);
 
+//global variable 'var email'
 var email = "tathompson6@hawkmail.hfcc.edu";
-
 console.log(email);
+
+//uses const COURSE_NAME to finish console log
+const COURSE_NAME = "CIS-172";
+console.log("I am using JS in " + COURSE_NAME);
+
+//loops ten times
+for(x=0;x<10;x++)
+    console.log("This is for loop # " + (x+1));
+
+//loops ten more times
+while(x<20) {
+    console.log("This is while loop # " + (x + 1));
+    x++;
+}
+
+//will compare string type my name to provided string
+if(myName === "Mr. Banks")
+    console.log("Welcome back Professor!");
+else
+    console.log("Welcome back " +myName + " - " + myMajor);
+
 
