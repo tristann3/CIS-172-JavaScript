2776ff2a503ed1030bc384a14df8d0fb591cf42c
Tristan Thompson <tristan15thompson@hawkmail.hfcc.edu>
2020-01-24 14:18:53 -0500

    Began testing console logs

---
+++ Homework/hw1/homework1-1-24.js
@@ -0,0 +1,12 @@
+let myName = "Tristan";
+let myMajor = "Assicisates in Applied Science in Computer Science";
+let funFact = "I have the same name as a professional athlete.";
+
+console.log(myName);
+console.log(myMajor);
+console.log(funFact);
+
+var email = "tathompson6@hawkmail.hfcc.edu";
+
+console.log(email);
+
