73252f4e70f4220442af9c305811766eb4b23473
Tristan Thompson <tristan15thompson@hawkmail.hfcc.edu>
2020-02-10 20:11:12 -0500

    Completed Homework 3 added comments
    Tristan Thompson
    2/10/2020

--- Homework/hw2/calculator.js
+++ Homework/hw2/calculator.js
@@ -1,19 +1,22 @@
+// global scoped so that equals function can be exectued in html
 var num1 = null;
 var num2 = null;
 var operator = null;
 var answer = null;
 
-//choose number function
+//fires when a number button is clicked
 function chooseNum(number) {
     if (num1 == null) {
         num1 = number;
     } else
         num2 = number;
 }
+//fires when an operator button is clicked
 //opChoice represents what the user chooses 1-add 2-subtract 3-multiply 4-divide
 function chooseOperator(opChoice) {
         operator = opChoice;
 }
+//fired when equals is clicked, checks for 3 params, will not execute if any of the three are absent
 function equals(num1, num2, operator) {
     if(num1!=null && num2!=null && num3!=null){
         switch(operator) {
@@ -36,7 +39,8 @@ function equals(num1, num2, operator) {
         }
     }
     else
-        console.log("you aint got nuff info for me to calculate your problems....you must have lots of those.......")
+        alert("Not enough variables");
+        //console.log("you aint got nuff info for me to calculate your problems....you must have lots of those.......")
 }
 function updateDisplay()
 {
