f48e2f720b488b885f890d9c80e6ac155947e539
Tristan Thompson <tristan15thompson@hawkmail.hfcc.edu>
2020-04-16 20:02:25 -0400

    Completed HW 6
    Tristan Thompson
    4/9/2020

--- Homework/hw6/index.js
+++ Homework/hw6/index.js
@@ -8,11 +8,30 @@ let PageHandler = {
             console.log("The Page Has Loaded!");
             if (jQuery)
                 console.log("jQuery Has Loaded!");
-            else
-                console.log("jQuery failure.");
         } catch(err){
-
+            console.log(err);
         }
+        //Select without jQuery
+        let div = document.getElementById("JohnCena");
+        // console.log("myDiv: ", div);
+
+        //Select with jQuery
+        PageHandler.pageDiv = $( '#JohnCena' );
+        PageHandler.pageDiv.hide();
+
+        let btnShowDiv = $('#btnShow');
+        btnShowDiv.click( PageHandler.showDiv );
+
+        let btnRedirect = $('#btnjQuery');
+        btnRedirect.click( PageHandler.redirect );
+    },
+    showDiv : function ( event )
+    {
+        PageHandler.pageDiv.show();
+    },
+    redirect : function (event )
+    {
+        window.open('https://api.jquery.com/');
     },
 };
 
