b3736fc002d524cba4726baf251ce18eabadd277
Tristan Thompson <tristan15thompson@hawkmail.hfcc.edu>
2020-04-23 17:13:26 -0400

    Completed HW 6
    Tristan Thompson
    4/9/2020

--- Homework/hw4/index.js
+++ Homework/hw4/index.js
@@ -6,11 +6,68 @@
 
 let PageManager =
     {
-        id:    tweetID.value ,
-        message:   tweetMessage.value ,
-        likes:  tweetLikes.value,
-        shares: tweetShares.value,
-        comments:   tweetComments.value,
-        date:   new Date(),
-        username:   tweetUsername.value,
+            init : function()
+            {
+                let btn1 = document.getElementById("btn1");
+                let btn2 = document.getElementById("btn2");
+                let btn3 = document.getElementById("btn3");
+                let txt1 = document.getElementById("txtInput1");
+                let txt2 = document.getElementById("txtInput2");
+                let myDiv = document.getElementById("divElement");
+                let dropdown = document.getElementById("colors");
+                btn1.addEventListener("click", PageManager.changeH1);
+                btn2.addEventListener("click", PageManager.appendDiv);
+                btn3.addEventListener("click", PageManager.logObject);
+                dropdown.addEventListener("change", PageManager.changeColor);
+            },
+            changeH1 : function(txt1)
+            {
+                    console.log("Loaded");
+                    var textBox = document.getElementById("txtInput1");
+                    var newText = textBox.value
+                    var myH1 = document.getElementById("myH1");
+                    myH1.innerHTML = newText;
+            },
+            appendDiv : function()
+            {
+                    console.log("Loaded Append function");
+                    var textBox = document.getElementById("txtInput2");
+                    var newText = textBox.value;
+                    document.getElementById("divElement").append(newText);
+                    // myDiv.appendChild(textBox.innerText);
+            },
+            logObject : function()
+            {
+                    console.log(PageManager);
+            },
+            changeColor : function()
+            {
+                    console.log("LOOOOOOAD");
+
+                    var dropdown = document.getElementById("colors");
+                    var newColor = colors.value;
+                    console.log("newColor: " + newColor);
+                    var bodyElement = document.body;
+                    if (newColor == "white")
+                    {
+                                 bodyElement.style.backgroundColor = "white";
+                    }
+                    else if (newColor == "black")
+                    {
+                                 bodyElement.style.backgroundColor = "black";
+                    }
+                    else if (newColor == "red")
+                    {
+                                bodyElement.style.backgroundColor = "red";
+                    }
+                    else if (newColor == "blue")
+                    {
+                                bodyElement.style.backgroundColor = "blue";
+                    }
+                    else
+                    {
+                                bodyElement.style.backgroundColor = "green";
+                    }
+            }
     };
+window.onload = PageManager.init;
